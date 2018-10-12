de31c81edd53915afe12be308ebf1392872be198
Morgen Stary <morgenstary@North-Pole.local>
2018-10-11 23:28:44 -0700

    working with bonus

--- liri.js
+++ liri.js
@@ -19,42 +19,39 @@ function processCommands() {
             spotifyThis();
             break;
         case 'movie-this':
-        movieThis();
+            movieThis();
             break;
         case 'do-what-it-says':
-        doWhatItSays();
+            doWhatItSays();
             break;
-            default:
-        console.log("Invalid command. Please type any of the following commnds: concert-this spotify-this-song movie-this or do-what-it-says");
+        default:
+            console.log("Invalid command. Please type any of the following commnds: concert-this spotify-this-song movie-this or do-what-it-says");
     }
 }
 
 function concertThis() {
-    
     var queryUrl = "https://rest.bandsintown.com/artists/" + commandParam + "/events?app_id=04986f7c93838994a17f468de0ad9a86";
-    
     request(queryUrl, function (error, response) {
-        
         if (!error && response.statusCode === 200) {
-            
             var data = JSON.parse(response.body);
             if (!data.length) {
-                console.log("There are no upcoming concerts for " + commandParam);
+                var output = "There are no upcoming concerts for " + commandParam;
+            } else {
+                for (var i = 0; i < data.length; i++) {
+                    var date = data[i].datetime;
+                    var format = 'L';
+                    var result = moment(date).format(format);
+
+                    //Put the whole enchalada in a variable
+                    var output = (" \n\n" + data[i].venue.name +
+                        "\n" + data[i].venue.city +
+                        "\n" + result);
+                    //Write to terminal/log.txt
+                    console.log(output);
+                    writeLog(output);
+                }
             }
-            
-            for (var i = 0; i < data.length; i++) {
-                
-                
-                console.log(" \n" + data[i].venue.name);
-                console.log(data[i].venue.city);
-                
-                var date = data[i].datetime;
-                var format = 'L';
-                var result = moment(date).format(format);
-                
-                console.log(result);
-            }
-            
+
         }
     });
 }
@@ -69,31 +66,34 @@ function spotifyThis() {
     spotify.search({
         type: "track",
         query: commandParam
-        
+
     },
-    function (error, data) {
-        if (error) {
-            console.log(error);
-        } else {
-            var tracks = data.tracks.items;
-            
-            for (var i = 0; i < tracks.length; i++) {
-                var artistArray = tracks[i].artists;
-                var artists = [];
-                for (var j = 0; j < artistArray.length; j++) {
-                    artists.push(artistArray[j].name);
-                    
+        function (error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                var tracks = data.tracks.items;
+
+                for (var i = 0; i < tracks.length; i++) {
+                    var artistArray = tracks[i].artists;
+                    var artists = [];
+                    for (var j = 0; j < artistArray.length; j++) {
+                        artists.push(artistArray[j].name);
+
+                    }
+
+                    //Put the whole enchalada in a variable
+                    var output = ("\n\nArtists: " + artists.join(", ") +
+                        "\nSong: " + tracks[i].name +
+                        "\nAlbum: " + tracks[i].album.name +
+                        "\nPreview: " + tracks[i].preview_url);
+                    //Write to terminal/log.txt
+                    console.log(output);
+                    writeLog(output);
+
                 }
-                console.log("\nArtists: " + artists.join(", "));
-                console.log("Song: " + tracks[i].name);
-                console.log("Album: " + tracks[i].album.name);
-                console.log("Preview: " + tracks[i].preview_url);
-                
-                
-                
             }
         }
-    }
     )
 
 }
@@ -104,31 +104,37 @@ function movieThis() {
     if (commandParam === "") {
         commandParam = "Mr.Nobody";
     }
-    var queryUrl = "http://www.omdbapi.com/?t=" + commandParam + "&y=&plot=short&apikey=trilogy";
-    
-    request(queryUrl, function(error, response, body) {
-        
-       
+    var queryUrl = "http://www.omdbapi.com/?t=" + commandParam + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+
+
         if (!error && response.statusCode === 200) {
-            
-            
-            console.log("\nTitle: " + JSON.parse(body).Title);
-            console.log("Release year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+
+            //Put the whole enchalada in a variable
+            var output = (" \n\nTitle: " + JSON.parse(body).Title +
+                "\nRelease year: " + JSON.parse(body).Year +
+                "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+                "\nProduced in: " + JSON.parse(body).Country +
+                "\nLanguages: " + JSON.parse(body).Language +
+                "\nPlot: " + JSON.parse(body).Plot +
+                "\nActors: " + JSON.parse(body).Actors);
+            //Write to terminal/log.txt
+            console.log(output);
+            writeLog(output);
+
+            // Omitted tomato rating, although it technically works omdb doesn't provide info
+            // leaving the value "N/A" after testing several movie titles
             // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-            console.log("Produced in: " + JSON.parse(body).Country);
-            console.log("Languages: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
-        } 
+        }
     });
-    
+
 }
 
 //    * `do-what-it-says`
 
 function doWhatItSays() {
-    
+
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
@@ -141,13 +147,24 @@ function doWhatItSays() {
     });
 }
 
+// function for writing to log file
+function writeLog(output) {
+    fs.appendFile("log.txt", output, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+    });
+}
+
 processCommands();
 
 
 
 
 
- 
+
+
 
 
 
